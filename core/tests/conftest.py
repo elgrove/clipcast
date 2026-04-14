4b19921047ddbb39ac62d804aad8250991530ea7
Aaron <aaron0lovegrove@gmail.com>
2026-04-14 23:12:03 +0100

    chore: remove v1 legacy code (Django, scripts, CI workflows)

--- core/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import django
-from django.conf import settings
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
-if not settings.configured:
-    django.setup()
